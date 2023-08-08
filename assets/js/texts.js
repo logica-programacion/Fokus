1823280e7ad58824076e946c4179002cecc92cc8
Mari Spinola <mariana.spinola@alura.com.br>
2023-08-08 18:23:18 -0300

    fix: explicacoes

--- assets/js/texts.js
+++ assets/js/texts.js
@@ -1,9 +1,14 @@
 function addDynamicText(text) {
+     // Seleciona o elemento da seção com a classe "text-wrapper"
     const textWrapper = document.querySelector('.text-wrapper');
+
+    // Cria um elemento <h1>
     const h1Element = document.createElement('h1');
 
+    //Acessando elemento <h1> e modificando o seu valor
     h1Element.textContent = text;
     
+    // Adiciona o elemento <h1> à seção
     textWrapper.appendChild(h1Element);
 }
 
