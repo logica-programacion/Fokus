8343fdd53b3628101d4cad28386d7ba400099aab
Mari Spinola <mariana.spinola@alura.com.br>
2023-08-09 19:48:09 -0300

    fix: style text wrapper and function

--- js/texts.js
+++ js/texts.js
@@ -1,18 +1,29 @@
-function addDynamicText(text) {
-     // Seleciona o elemento da seção com a classe "text-wrapper"
-    const textWrapper = document.querySelector('.text-wrapper');
-
-    // Cria um elemento <h1>
+function renderDynamicContent(normalText, boldText) {
+    // Seleciona o elemento <section> com a classe "text-wrapper"
+    const sectionElement = document.querySelector(".text-wrapper");
+    
+    // Criação dos elementos <h1> e <span>
     const h1Element = document.createElement('h1');
-
-    //Acessando elemento <h1> e modificando o seu valor
-    h1Element.textContent = text;
-
-    // Adiciona o elemento <h1> à seção
-    textWrapper.appendChild(h1Element);
+    h1Element.classList.add('text-wrapper-normal');
+    
+    const spanElement = document.createElement('span');
+    spanElement.classList.add('text-wrapper-bold');
+    
+    // Definir os textos dos elementos
+    h1Element.textContent = normalText;
+    spanElement.textContent = boldText;
+    
+    // Anexar o <span> ao <h1>
+    h1Element.appendChild(spanElement);
+    
+    // Limpar o conteúdo anterior da seção
+    sectionElement.innerHTML = '';
+    
+    // Anexar o <h1> à seção
+    sectionElement.appendChild(h1Element);
 }
 
-// Chamando a função e passando o texto como parâmetro
-addDynamicText("Optimiza tu productividad, sumérgete en lo que importa.");
-// addDynamicText("¿Qué tal tomar un respiro? ¡Tómate un breve descanso!");
-// addDynamicText("Es hora de volver a la superficie. Toma un largo descanso.");
+// Chamando a função para renderizar o conteúdo
+renderDynamicContent("Optimiza tu productividad", " sumérgete en lo que importa.");
+
+// renderDynamicContent("¿Qué tal tomar un respiro?", " ¡Tómate un breve descanso!");
